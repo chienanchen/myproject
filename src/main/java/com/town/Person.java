1ff4e851e8704bdd3c1fde8e8b69a94f624516c8
Chien-An <anguschen0526@gmail.com>
2019-01-16 11:36:34 +0800

    Initial commit

---
+++ src/main/java/com/town/Person.java
@@ -0,0 +1,8 @@
+package com.town;
+
+public class Person {
+    public void hello(){
+        System.out.println("hello");
+    }
+
+}
---
+++ src/main/kotlin/com/tom/hello.kt
@@ -0,0 +1,14 @@
+package com.tom
+
+fun main(args: Array<String>) {
+    //println("hello kotlin")
+    //Human().Happy()
+    val h = Human()
+    h.Happy()
+
+}
+class Human {
+    fun Happy(){
+        println("hello kotlin")
+    }
+}
